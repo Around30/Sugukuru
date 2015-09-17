d275afc37f03107b7cc5eed9581ee78e970fb07f
truthNH <truth.nh@hotmail.co.jp>
2015-09-17 14:12:18 +0900

    Add 法人登録サーブレット

---
+++ src/jp/ac/hal/Controller/CorporationRegist.java
@@ -0,0 +1,114 @@
+package jp.ac.hal.Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sun.lwawt.macosx.CPopupMenu;
+
+/**
+ * Servlet implementation class CorporationRegist
+ */
+@WebServlet("/CorporationRegist")
+public class CorporationRegist extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CorporationRegist() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		//パラメータ受け取り
+		
+		String corporationName = request.getParameter("corporationName");
+		String postalCode = request.getParameter("postalCode");
+		String address = request.getParameter("address");
+		String phoneNumber = request.getParameter("address");
+		String passwd = request.getParameter("passwd");
+		String creditLimit = request.getParameter("creditLimit");
+		
+		corporationName = "aaa";
+		postalCode = "3321111";
+		address = "aaaaaaa";
+		phoneNumber = "22222222";
+		passwd = "adaaaaaaa";
+		creditLimit = "4444444";
+		
+		//入力項目チェック
+		InputCheck i = new InputCheck();
+		boolean err = false;
+		err |= i.checkNullChar(corporationName, postalCode, address, phoneNumber, passwd, creditLimit);
+		err |= i.checkCharaLength(corporationName, 30);
+		err |= i.checkCharaLength(postalCode, 7);
+		err |= i.checkCharaLength(address, 100);
+		err |= i.checkCharaLength(phoneNumber, 11);
+		err |= i.checkCharaLength(passwd, 50);
+		err |= i.checkCharaLength(creditLimit, 8);
+		err |= i.checkNumbers(postalCode);
+		err |= i.checkNumbers(phoneNumber);
+		err |= i.checkNumbers(creditLimit);
+		
+		System.out.println(err);
+		
+		if(!err) {
+			//登録処理
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		//パラメータ受け取り
+		
+		String corporationName = request.getParameter("corporationName");
+		String postalCode = request.getParameter("postalCode");
+		String address = request.getParameter("address");
+		String phoneNumber = request.getParameter("address");
+		String passwd = request.getParameter("passwd");
+		String creditLimit = request.getParameter("creditLimit");
+		
+		corporationName = "aaa";
+		postalCode = "3321111";
+		address = "aaaaaaa";
+		phoneNumber = "22222222";
+		passwd = "adaaaaaaa";
+		creditLimit = "100000";
+		
+		//入力項目チェック
+		InputCheck i = new InputCheck();
+		boolean err = false;
+		err |= i.checkNullChar(corporationName, postalCode, address, phoneNumber, passwd, creditLimit);
+		err |= i.checkCharaLength(corporationName, 30);
+		err |= i.checkCharaLength(postalCode, 7);
+		err |= i.checkCharaLength(address, 100);
+		err |= i.checkCharaLength(phoneNumber, 11);
+		err |= i.checkCharaLength(passwd, 50);
+		err |= i.checkCharaLength(creditLimit, 8);
+		err |= i.checkNumbers(postalCode);
+		err |= i.checkNumbers(phoneNumber);
+		err |= i.checkNumbers(creditLimit);
+		
+		System.out.println(err);
+		
+		if(!err) {
+			//登録処理
+		}
+	}
+}
