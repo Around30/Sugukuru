721a5b269f26988a069de7c5272ff3fcd186aa5a
redisczoho <redisc@zoho.com>
2015-09-28 00:07:51 +0900

    add ic

--- src/jp/ac/hal/Util/Ic.java
+++ src/jp/ac/hal/Util/Ic.java
@@ -20,7 +20,7 @@ public class Ic
 		{
 			return null;
 		}
-		else if(max <= str.length())
+		else if(max < str.length())
 		{
 			throw new IllegalArgumentException();
 		}
@@ -43,6 +43,74 @@ public class Ic
 		else
 		{
 			return str;
+		}
+	}
+	
+	public static String stringNotNullable(String str)
+	{
+		if(str == null || str.isEmpty())
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			return str;
+		}
+	}
+	
+	public static String stringNotNullable(String str, int max)
+	{
+		if(str == null || str.isEmpty())
+		{
+			throw new IllegalArgumentException();
+		}
+		else if(max < str.length())
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			return str;
+		}
+	}
+	
+	public static String stringNotNullable(String str, int max, int min)
+	{
+		if(str == null || str.isEmpty())
+		{
+			throw new IllegalArgumentException();
+		}
+		else if(str.length() < min || max < str.length())
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			return str;
+		}
+	}
+	
+	public static Integer intNullable(String str)
+	{
+		if(str == null || str.isEmpty())
+		{
+			return null;
+		}
+		else
+		{
+			return Integer.parseInt(str);
+		}
+	}
+
+	public static Integer intNotNullable(String str)
+	{
+		if(str == null || str.isEmpty())
+		{
+			throw new IllegalArgumentException();
+		}
+		else
+		{
+			return Integer.parseInt(str);
 		}
 	}
 }
