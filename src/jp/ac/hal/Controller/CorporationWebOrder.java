2444b17370cbcb33f72f942206bd15e47b89ebf9
redisczoho <redisc@zoho.com>
2015-09-28 07:46:00 +0900

    cart

--- src/jp/ac/hal/Controller/CorporationWebOrder.java
+++ src/jp/ac/hal/Controller/CorporationWebOrder.java
@@ -1,25 +1,17 @@
 package jp.ac.hal.Controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import jp.ac.hal.Dao.Dao;
-import jp.ac.hal.Model.Corporation;
-import jp.ac.hal.Model.CorporationOrder;
-import jp.ac.hal.Model.Order;
-import jp.ac.hal.Util.Ic;
 
 /**
- * Servlet implementation class OrderServlet
+ * Servlet implementation class CorporationWebOrder
  */
-@WebServlet("/OrderServlet")
-public class Cart extends HttpServlet {
+@WebServlet("/CorporationWebOrder")
+public class CorporationWebOrder extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -28,14 +20,7 @@ public class Cart extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try
 		{
-			HttpSession s = request.getSession();
-			if(s.getAttribute("orderId") == null)
-			{
-				Order o = new Order();
-				o.setCart(true);
-				int id = Dao.getInstance().executeInsert(o);
-				s.setAttribute("orderId", id);
-			}
+			
 		}
 		catch(Exception e)
 		{
