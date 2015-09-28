909e1d98d8f5431ebafb82adce1466b2d1b937a9
truthNH <truth.nh@hotmail.co.jp>
2015-09-28 16:24:51 +0900

    Fix ProductList.java to show all products

--- src/jp/ac/hal/Controller/ProductsList.java
+++ src/jp/ac/hal/Controller/ProductsList.java
@@ -1,6 +1,7 @@
 package jp.ac.hal.Controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,10 +45,8 @@ public class ProductsList extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-
 		// 受け取る文字コードの設定
-				request.setCharacterEncoding("UTF-8");
-
+		request.setCharacterEncoding("UTF-8");
 		//転送先
 		String sendURL = "商品一覧.jsp";
 		//エラーフラグ
@@ -55,43 +54,10 @@ public class ProductsList extends HttpServlet {
 		//メッセージ格納用List
 		ArrayList<String> msg = new ArrayList<String>();
 		List<Object[]> pList = new ArrayList<>();
-//		List<Product> pL = new ArrayList<>();
-
-//				//エラーなし
-//				if (!err) {
-			try {
-				Dao dao = Dao.getNewInstance();
-
-//					for(int i = 0; i < dao.商品数 ; i++){
-//					List<Object[]> pList = dao.全product情報引っ張ってくるselect文;
-
-
-
-//				}
-
-			} catch (NamingException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
-				msg.add( "DB処理でエラーが発生しました。");
-			}
-//				}
-//				//エラーあり
-//				else {
-//					sendURL = "index.jsp";
-//					//メッセージ転送
-//					request.setAttribute("msg", msg);
-//					request.setAttribute("err", err);
-//
-//				}
-
 		RequestDispatcher disp = request.getRequestDispatcher(sendURL);
-
 		//文字コード
 		response.setContentType("text/html; charset=UTF-8");
 		request.setAttribute("pList", pList);
-
 		disp.forward(request, response);
-
 	}
-
 }
